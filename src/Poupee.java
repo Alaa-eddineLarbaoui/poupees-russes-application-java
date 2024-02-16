41ff618286eb8db0a0903a4597005229b6b15dcd
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-02-16 17:08:23 +0100

    j'ai creer un constructeur et le methodes nécessaires

--- src/Poupee.java
+++ src/Poupee.java
@@ -1,12 +1,40 @@
 abstract public class Poupee {
+    // les propriétés
+    private int taille;
+    private boolean ouverte;
 
-abstract public void ouvrir();
 
-abstract public void fermer();
+    // constructeur
+    Poupee(int tailleP){
+        this.taille= tailleP;
+        this.ouverte = false;
+    }
 
-abstract public void placerDans();
+    // les getter et setter
+    public int getTaille() {
+        return taille;
+    }
 
-abstract public void  sortirDe();
+    public void setTaille(int taille) {
+        this.taille = taille;
+    }
+
+    public boolean isOuverte() {return ouverte;
+    }
+
+    public void setOuverte(boolean ouverte) {
+        this.ouverte = ouverte;
+    }
+
+
+    // méthodes abstraites
+    public abstract  void ouvrir();
+
+    public abstract void fermer();
+
+    public abstract void placerDans(Poupee p);
+
+    public abstract void sortirDe(Poupee p);
 
 
 }
--- src/PoupeeRusse.java
+++ src/PoupeeRusse.java
@@ -1,22 +1,61 @@
+import java.sql.SQLOutput;
+
 public class PoupeeRusse extends Poupee{
+
+    Poupee poupee;
+
+    PoupeeRusse(int tailleP) {
+        super(tailleP);
+        poupee = null;
+    }
 
     @Override
     public void ouvrir() {
-
+        if (isOuverte() == false){
+            setOuverte(true);
+            System.out.println("la poupee a ete ouverte");
+        }
+        else {
+            System.out.println("est deja ouverte");
+        }
     }
 
     @Override
     public void fermer() {
-
+        if (isOuverte() == true){
+            setOuverte(false);
+            System.out.println("la poupee a ete fermer");
+        }
+        else {
+            System.out.println("est deja fermer");
+        }
     }
 
     @Override
-    public void placerDans() {
-
+    public void placerDans(Poupee p) {
+        if (p.getTaille() <= getTaille()){
+            System.out.println("impossible");
+        }
+        else if (isOuverte() == false){
+            System.out.println("ouvrir tout d'abord la popee");
+        }
+        else {
+            poupee = p;
+            System.out.println("placemment de la poupee");
+        }
     }
 
     @Override
-    public void sortirDe() {
-
+    public void sortirDe(Poupee p) {
+        if (isOuverte()==false){
+            System.out.println("la popee est fermer,ouvrir tout d'abord la poupee");
+        }
+        else if (p != poupee){
+            System.out.println("la poupee est vide  ");
+        }
+        else {
+           poupee = null;
+            System.out.println(" la poupee est sortie");
+        }
     }
 }
