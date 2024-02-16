06461d5615a476e1f3e3acf288441303fe2d0035
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-02-16 09:42:29 +0100

    j'ai creer la classe poupee et la classe PoupeeRusse

---
+++ src/Poupee.java
@@ -0,0 +1,12 @@
+abstract public class Poupee {
+
+abstract public void ouvrir();
+
+abstract public void fermer();
+
+abstract public void placerDans();
+
+abstract public void  sortirDe();
+
+
+}
---
+++ src/PoupeeRusse.java
@@ -0,0 +1,22 @@
+public class PoupeeRusse extends Poupee{
+
+    @Override
+    public void ouvrir() {
+
+    }
+
+    @Override
+    public void fermer() {
+
+    }
+
+    @Override
+    public void placerDans() {
+
+    }
+
+    @Override
+    public void sortirDe() {
+
+    }
+}
